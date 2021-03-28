d6a21427e70e662343e2b12fd6ff4e764926861a
BooTheBomb01 <68622251+BooTheBomb01@users.noreply.github.com>
2021-03-28 14:17:29 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -13,19 +13,17 @@ function serveStaticFile(res, path, contentType, responseCode = 200) {
   })
 }
 
-
 const server = http.createServer((req,res) => {
-    // normalize url by removing querystring, optional trailing slash, and
-    // making lowercase
-    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
-    switch(path) {
-      case '':
-        serveStaticFile(res, '/Public/Index.html', 'text/html')
-        break
-      
-    }
-  })
+  // normalize url by removing querystring, optional trailing slash, and
+  // making lowercase
+  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+  switch(path) {
+    case '':
+      serveStaticFile(res, '/Public/Index.html', 'text/html')
+      break
+    
+  }
+})
 
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+server.listen(port, () => console.log(`server started on port ${port}; ` +
+  'press Ctrl-C to terminate....'))
